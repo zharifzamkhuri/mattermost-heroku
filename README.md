62a3be3030af575a984d7b1d7bd0a78e8bf2d017
Christopher De Cairos <cade@mozillafoundation.org>
2016-01-26 17:36:53 -0500

    Rewrite buildpack to allow source based builds and deploys to heroku without any changes to the mattermost/platform repo.
    You can now tell the buildpack which mattermost repo and branch/tag to clone.

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Mattermost + inline buildpack == Mattermost on Heroku
+# Build and deploy mattermost from source to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+This buildpack is an [inline buildpack](https://github.com/kr/heroku-buildpack-inline/) (tldr: this repo deploys to Heroku and uses itself as a buildpack) for deploying [Mattermost](https://mattermost.org) to [Heroku](https://heroku.com).
+You could say it's a hybrid of the Node and Go buildpacks, specifically designed to set up a build environment for Mattermost, and comes with a startup script that writes your Heroku environment variables to a config file when the dyno starts up (because Mattermost is not a [12 factor app](http://12factor.net/config) .... yet)
+
+## Rebuilding
+
+Due to the nature of this being an inline buildpack, You have two options if you want to deploy a new version of Mattermost:
+
+1. create an empty git commit - `git commit --allow-empty -m "empty commit"` and push it to the Heroku app repo
+2. (preferred) use the [Build and Release API](https://devcenter.heroku.com/articles/build-and-release-using-the-api#creating-builds)
+  * [httpie example](https://github.com/jakubroztocil/httpie): `http post https://api.heroku.com/apps/app-name/builds source_blob:='{"source_blob":{"url":"https://github.com/heroku/node-js-sample/archive/master.tar.gz", "version": "cb6999d361a0244753cf89813207ad53ad906a14"}}' Accept:"application/vnd.heroku+json; version=3" `
 
 ## Warnings
 
@@ -7,6 +20,7 @@
 2. Not using s3 means any uploads will be lost on dyno restart or application reconfiguration or redeploy and won't be consistent across multiple dynos.
    Without s3 backing this is not anything more than a one time demo.
 
-## ToDo
+## To Do
 
-1. More configurable options
+* Add more config options
+* Cache Node tarball
--- lib/deps.sh
+++ lib/deps.sh
@@ -1,3 +1,5 @@
+# Sourced by bin/compile
+
 get_os() {
   uname | tr A-Z a-z
 }
@@ -10,19 +12,22 @@ get_cpu() {
   fi
 }
 
-os=$(get_os)
-cpu=$(get_cpu)
+get_platform() {
+  ARCH=$(uname -m|tr A-Z a-z)
+
+  if test $ARCH = x86_64
+  then ARCH=amd64
+  fi
+
+  echo "$(uname|tr A-Z a-z)-$ARCH"
+}
 
 install_nodejs() {
-  local dir="$1"
-
-  echo "Downloading and installing node 5.1.0..."
-  local download_url="http://s3pository.heroku.com/node/v5.1.0/node-v5.1.0-$os-$cpu.tar.gz"
-  curl "$download_url" --silent --fail -o /tmp/node.tar.gz || (echo "Unable to download node $version; does it exist?" && false)
+  curl "$NODE_DOWNLOAD_URL" --silent --fail -o /tmp/node.tar.gz || (step "Unable to download node $NODE_VERSION; does it exist?" && false)
   tar xzf /tmp/node.tar.gz -C /tmp
-  rm -rf $dir/*
-  mv /tmp/node-v5.1.0-$os-$cpu/* $dir
-  chmod +x $dir/bin/*
+  rm -rf $NODE_BUILD_DIR/*
+  mv /tmp/node-$NODE_VERSION-$OS-$CPU/* $NODE_BUILD_DIR
+  chmod +x $NODE_BUILD_DIR/bin/*
 }
 
 platext() {
@@ -48,38 +53,28 @@ urlfor() {
 }
 
 install_go() {
-    ver=$1
-    cache=$2
-
-    file=${GOFILE:-$ver.$(uname|tr A-Z a-z)-amd64$(platext $ver).tar.gz}
-    url=${GOURL:-$(urlfor $ver $file)}
-
-    if test -d $cache/$ver/go
+    if test -d $CACHE_DIR/$GOVERSION/go
     then
-        step "Using $ver"
+        echo "Using cached version: $GOVERSION"
     else
-        rm -rf $cache/* # be sure not to build up cruft
-        mkdir -p $cache/$ver
-        cd $cache/$ver
-        start "Installing $ver"
-            curl -sO $url
-            tar zxf $file
-            rm -f $file
-        finished
+        rm -rf $CACHE_DIR/* # be sure not to build up cruft
+        mkdir -p $CACHE_DIR/$GOVERSION
+        cd $CACHE_DIR/$GOVERSION
+        echo "Installing $GOVERSION"
+            curl -sO $GODOWNLOADURL
+            tar zxf $GOFILENAME
+            rm -f $GOFILENAME
         cd - >/dev/null
     fi
 }
 
 install_compass() {
-  ruby_cache_dir=$1
-  ruby_gem_dir=$2
-
-  if test -e $ruby_cache_dir/bin/compass
+  if test -e $RUBY_CACHE_DIR/bin/compass
   then
-      cp -R $ruby_cache_dir/* $ruby_gem_dir
+      cp -R $RUBY_CACHE_DIR/* $RUBY_GEM_DIR
       step "using compass 1.0.3"
   else
       # Install compass gem
-      gem install --install-dir $ruby_gem_dir -v 1.0.3 compass
+      gem install --install-dir $RUBY_GEM_DIR -v 1.0.3 compass
   fi
 }
