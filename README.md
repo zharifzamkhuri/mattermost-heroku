b9547b49effc0e777837123513392650401efb38
Christopher DeCairos <cade@mozillafoundation.org>
2016-10-05 13:01:24 -0400

    Add more config options (#2)
    * initial work adding in all environment variables present in Mattermost 3.4 Team and Enterprise editions
    
    * fix wrong config variable
    
    * fix error in the start script
    
    * add default value for RESTRICT_DIRECT_MESSAGE
    
    * add default for DRIVER_NAME
    
    * update buildpack tag in app.json

--- README.md
+++ README.md
@@ -9,71 +9,20 @@ Mattermost is not a [12 factor compatible app](http://12factor.net/config), so t
 
 ### Known to work with Mattermost 3.4.0 Team and Enterprise editions
 
+## 
+
 ## Configuration options
 
-### Building
+### Buildpack Specific Variables
 
 Set `MATTERMOST_VERSION` to the release version you'd like to install. i.e. '3.4.0'
 Set `MATTERMOST_TYPE` to either 'team' or 'enterprise'
 
-### Mattermost
+### Mattermost Configuration
 
-[Check out the configuration docs for more info](https://docs.mattermost.com/administration/config-settings.html)
+Mattermost-Heroku supports every config option that Mattermost 3.4 defines. You can see the mapping between config options and Heroku environment variables in the [start.sh file](/start.sh).
 
-|Variable|Purpose|type|default|
-|--------|-------|----|-------|
-|MATTERMOST_PORT|The port you want Nginx and Mattermost to talk to each other on|int|8080|
-|ENABLE_OUTGOING_WEBHOOKS|Enable the creation of outgoing webhooks|bool|false|
-|ENABLE_POST_USERNAME_OVERRIDE|Allow posts to override the username field, useful for webhook integrations|bool|false|
-|ENABLE_POST_ICON_OVERRIDE|Allow posts to override the icon field, useful for webhook integrations|bool|false|
-|ENABLE_COMMANDS|Enable custom slash commands|bool|false|
-|ENABLE_2FA|Enable Two Factor Authentication (Enterprise only)|bool|false|
-|TEAM_SETTINGS__SITE_NAME|The name of your Mattermost site|string|?|
-|MAX_USERS_PER_TEAM|Maximum users per team|int|50|
-|ENABLE_TEAM_CREATION|Allow new teams to be created|bool|false|
-|RESTRICT_CREATION_TO_DOMAINS|Teams and user accounts can only be created by a verified email from this list of comma-separated domains|string|undefined|
-|ENABLE_TEAM_LISTING|Teams that are configured to appear in the team directory will appear on the system main page|bool|false|
-|ENABLE_OPEN_SERVER|Users can sign up to the server from the root page without an invite|bool|false|
-|RESTRICT_PUBLIC_CHANNEL_MANAGEMENT|Restrict the permission levels required to create, delete, rename, and set the header or purpose for public channels (Enterprise only)|bool|false|
-|DATABASE_URL|Database connection string|string||
-|MAX_IDLE_CONNS|Maximum number of idle connections held open to the database|int|10|
-|MAX_OPEN_CONNS|Maximum number of open connections held open to the database|int|10|
-|SQL_SETTINGS__AT_REST_ENCRYPT_KEY|32-character salt available to encrypt and decrypt sensitive fields in database|string||
-|LOG_LEVEL|Level of detail at which log events are written to the console. DEBUG, ERROR, INFO|string|INFO|
-|ENABLE_FILE|Enable or disable logging to file|bool|false|
-|FILE_SETTINGS__DRIVER_NAME|set the database driver(mysql or postgres)|string||
-|FILE_SETTINGS__PUBLIC_LINK_SALT|32-character salt added to signing of public image links|string||
-|FILE_SETTINGS__AMAZON_S3_ACCESS_KEY_ID|key for writing to S3|string||
-|FILE_SETTINGS__AMAZON_S3_SECRET_ACCESS_KEY|secret key for writing to S3|string||
-|FILE_SETTINGS__AMAZON_S3_BUCKET|S3 bucket to store uploaded content|string||
-|FILE_SETTINGS__AMAZON_S3_REGION|S3 region the bucket is located in|string||
-|ENABLE_SIGNUP_WITH_EMAIL|Allow team creation and account signup using email and password(set false for LDAP/OAuth signups only)|bool|false|
-|SEND_EMAIL_NOTIFICATIONS|Enables sending of email notifications|bool|false|
-|REQUIRE_EMAIL_VERIFICATION|Require email verification after account creation prior to allowing login|bool|false|
-|FEEDBACK_NAME|Name displayed on email account used when sending notification emails from Mattermost system|string||
-|FEEDBACK_EMAIL|Address displayed on email account used when sending notification emails from Mattermost system|string||
-|SMTP_USERNAME|username for the SMTP server sending notification emails|string||
-|SMTP_PASSWORD|password for the SMTP server sending notification emails|string||
-|SMTP_SERVER|hostname for the SMTP server sending notification emails|string||
-|SMTP_PORT|port number for the SMTP server sending notification emails|string||
-|CONNECTION_SECURITY|Set to TLS to send email over a secure connection|string||
-|EMAIL_SETTINGS__INVITE_SALT|32-character salt added to signing of email invites|string||
-|EMAIL_SETTINGS__PASSWORD_RESET_SALT|32-character salt added to signing of password reset emails|string||
-|SEND_PUSH_NOTIFICATIONS|Send push notifications using the configured push notification server|bool|false|
-|PUSH_NOTIFICATION_SERVER|The push notification server to use|string||
-|PUSH_NOTIFICATION_CONTENTS|The contents push notifications should have (full, generic)|string|full|
-|VARY_BY_REMOTE_ADDR|Rate limit API access by IP address|bool|false|
-|VARY_BY_HEADER|Vary rate limiting by HTTP header field specified|string||
-|TERMS_OF_SERVICE_LINK|Link to site's TOS|string||
-|PRIVACY_POLICY_LINK|Link to site's privacy policy|string||
-|ABOUT_LINK|Link to site's about page|string||
-|HELP_LINK|Link to help content|string||
-|REPORT_PROBLEM_LINK|link to report issues|string||
-|SUPPORT_EMAIL|email to send support requests to|string||
-|ENABLE_CUSTOM_BRAND|Enable custom branding|bool|false|
-|CUSTOM_BRAND_TEXT|Markdown formated string to desplay with custom branding|string||
-|CUSTOM_DESCRIPTION_TEXT|String of text describing the server|string|All team communication in one place, searchable and accessible anywhere|
-
+ [Check out the Mattermost configuration documentation for detailed information about each option.](https://docs.mattermost.com/administration/config-settings.html)
 
 ## Rebuilding
 
@@ -88,8 +37,3 @@ Use the deploy menu on the Heroku dashboard to trigger manual deploys.
 
 ## Enterprise Edition
 Activate it as instructed in the docs https://docs.mattermost.com/install/ee-install.html
-
-## To Do
-
-* Add more config options
-* Cache Node tarball
--- start.sh
+++ start.sh
@@ -2,17 +2,34 @@
 
 [[ ${FILE_SETTINGS__DRIVER_NAME} != "local" ]] || echo "WARNING!!!!! FILES ARE STORED ON DISK AND WILL BE REGULARLY REMOVED, THIS MEANS UPLOADS WILL DISAPEAR!!"
 
-###################################################################################
-# Apply some defaults values, and remove quotes that Heroku adds to truthy values #
-###################################################################################
+###############################################################################################
+# Apply some defaults values, and remove quotes that Heroku adds to truthy and numeric values #
+###############################################################################################
 
 ####################
 # Service Settings #
 ####################
-export ENABLE_OUTGOING_WEBHOOKS=`echo ${ENABLE_OUTGOING_WEBHOOKS:=false} | tr -d \"`
+export MAXIMUM_LOGIN_ATTEMPTS=`echo ${MAXIMUM_LOGIN_ATTEMPTS:=10} | tr -d \"`
+export ENABLE_OAUTH_SERVICE_PROVIDER=`echo ${ENABLE_OAUTH_SERVICE_PROVIDER:=false} | tr -d \"`
+export ENABLE_INCOMING_WEBHOOKS=`echo ${ENABLE_INCOMING_WEBHOOKS:=true} | tr -d \"`
+export ENABLE_OUTGOING_WEBHOOKS=`echo ${ENABLE_OUTGOING_WEBHOOKS:=true} | tr -d \"`
+export ENABLE_COMMANDS=`echo ${ENABLE_COMMANDS:=true} | tr -d \"`
+export ENABLE_ONLY_ADMIN_INTEGRATIONS=`echo ${ENABLE_ONLY_ADMIN_INTEGRATIONS:=true} | tr -d \"`
 export ENABLE_POST_USERNAME_OVERRIDE=`echo ${ENABLE_POST_USERNAME_OVERRIDE:=false} | tr -d \"`
 export ENABLE_POST_ICON_OVERRIDE=`echo ${ENABLE_POST_ICON_OVERRIDE:=false} | tr -d \"`
+export ENABLE_TESTING=`echo ${ENABLE_TESTING:=false} | tr -d \"`
+export ENABLE_DEVELOPER=`echo ${ENABLE_DEVELOPER:=false} | tr -d \"`
+export ENABLE_SECURITY_FIX_ALERT=`echo ${ENABLE_SECURITY_FIX_ALERT:=true} | tr -d \"`
+export ENABLE_INSECURE_OUTGOING_CONNECTIONS=`echo ${ENABLE_INSECURE_OUTGOING_CONNECTIONS:=false} | tr -d \"`
+export ENABLE_2FA=`echo ${ENABLE_2FA:=false} | tr -d \"`
+export ENABLE_CUSTOM_EMOJI=`echo ${ENABLE_CUSTOM_EMOJI:=false} | tr -d \"`
 
+export SESSION_LENGTH_WEB_IN_DAYS=`echo ${SESSION_LENGTH_WEB_IN_DAYS:=30} | tr -d \"`
+export SESSION_LENGTH_MOBILE_IN_DAYS=`echo ${SESSION_LENGTH_MOBILE_IN_DAYS:=30} | tr -d \"`
+export SESSION_LENGTH_SSO_IN_DAYS=`echo ${SESSION_LENGTH_SSO_IN_DAYS:=30} | tr -d \"`
+export SESSION_CACHE_IN_MINUTES=`echo ${SESSION_CACHE_IN_MINUTES:=10} | tr -d \"`
+export WEBSOCKET_SECURE_PORT=`echo ${WEBSOCKET_SECURE_PORT:=443} | tr -d \"`
+export WEBSOCKET_PORT=`echo ${WEBSOCKET_PORT:=80} | tr -d \"`
 
 #################
 # Team Settings #
@@ -21,61 +38,113 @@ export ENABLE_TEAM_CREATION=`echo ${ENABLE_TEAM_CREATION:=false} | tr -d \"`
 export ENABLE_USER_CREATION=`echo ${ENABLE_USER_CREATION:=false} | tr -d \"`
 export ENABLE_TEAM_LISTING=`echo ${ENABLE_TEAM_LISTING:=false} | tr -d \"`
 export ENABLE_OPEN_SERVER=`echo ${ENABLE_OPEN_SERVER:=false} | tr -d \"`
-export RESTRICT_PUBLIC_CHANNEL_MANAGEMENT=${RESTRICT_PUBLIC_CHANNEL_MANAGEMENT:="all"}
 export ENABLE_CUSTOM_BRAND=`echo ${ENABLE_CUSTOM_BRAND:=false} | tr -d \"`
-export CUSTOM_BRAND_TEXT=${CUSTOM_BRAND_TEXT}
-export CUSTOM_DESCRIPTION_TEXT=${CUSTOM_DESCRIPTION_TEXT}
-
-
-####################
-# Support Settings #
-####################
-export TERMS_OF_SERVICE_LINK=${TERMS_OF_SERVICE_LINK}
-export PRIVACY_POLICY_LINK=${PRIVACY_POLICY_LINK}
-export ABOUT_LINK=${ABOUT_LINK}
-export HELP_LINK=${HELP_LINK}
-export REPORT_PROBLEM_LINK=${REPORT_PROBLEM_LINK}
-export SUPPORT_EMAIL=${SUPPORT_EMAIL}
-export ENABLE_COMMANDS=`echo ${ENABLE_COMMANDS:=false} | tr -d \"`
-export ENABLE_2FA=`echo ${ENABLE_2FA:=false} | tr -d \"`
-
+export MAX_USERS_PER_TEAM=`echo ${MAX_USERS_PER_TEAM:=50} | tr -d \"`
+export RESTRICT_TEAM_NAMES=`echo ${RESTRICT_TEAM_NAMES:=true} | tr -d \"`
+export USER_STATUS_AWAY_TIMEOUT=`echo ${USER_STATUS_AWAY_TIMEOUT:=300} | tr -d \"`
+export RESTRICT_DIRECT_MESSAGE=`echo ${RESTRICT_DIRECT_MESSAGE:="any"}`
 
 ################
 # SQL Settings #
 ################
+export DRIVER_NAME=`echo ${DRIVER_NAME:="postgres"}`
 export SQL_SETTINGS__AT_REST_ENCRYPT_KEY=$(printf "${SQL_SETTINGS__AT_REST_ENCRYPT_KEY}%.0s" {1..32} | cut -c -32)
-
+export MAX_IDLE_CONNS=`echo ${MAX_IDLE_CONNS:=10} | tr -d \"`
+export MAX_OPEN_CONNS=`echo ${MAX_OPEN_CONNS:=10} | tr -d \"`
+export TRACE=`echo ${TRACE:=false} | tr -d \"`
 
 ################
 # Log Settings #
 ################
-export LOG_LEVEL=${LOG_LEVEL:="DEBUG"}
+export ENABLE_CONSOLE=`echo ${ENABLE_CONSOLE:=true} | tr -d \"`
 export ENABLE_FILE=`echo ${ENABLE_FILE:=false} | tr -d \"`
-
+export ENABLE_WEBHOOK_DEBUGGING=`echo ${ENABLE_WEBHOOK_DEBUGGING:=true} | tr -d \"`
+export ENABLE_DIAGNOSTICS=`echo ${ENABLE_DIAGNOSTICS:=true} | tr -d \"`
 
 #################
 # File Settings #
 #################
 export FILE_SETTINGS__PUBLIC_LINK_SALT=$(printf "${FILE_SETTINGS__PUBLIC_LINK_SALT}%.0s" {1..32} | cut -c -32)
 
+export MAX_FILE_SIZE=`echo ${MAX_FILE_SIZE:=52428800} | tr -d \"`
+export ENABLE_PUBLIC_LINK=`echo ${ENABLE_PUBLIC_LINK:=false} | tr -d \"`
+export THUMBNAIL_WIDTH=`echo ${THUMBNAIL_WIDTH:=120} | tr -d \"`
+export THUMBNAIL_HEIGHT=`echo ${THUMBNAIL_HEIGHT:=100} | tr -d \"`
+export PREVIEW_WIDTH=`echo ${PREVIEW_WIDTH:=1024} | tr -d \"`
+export PREVIEW_HEIGHT=`echo ${PREVIEW_HEIGHT:=0} | tr -d \"`
+export PROFILE_WIDTH=`echo ${PROFILE_WIDTH:=128} | tr -d \"`
+export PROFILE_HEIGHT=`echo ${PROFILE_HEIGHT:=128} | tr -d \"`
+export FILE_SETTINGS__AMAZON_S3_LOCATION_CONSTRAINT=`echo ${FILE_SETTINGS__AMAZON_S3_LOCATION_CONSTRAINT:=false} | tr -d \"`
+export FILE_SETTINGS__LOWERCASE_BUCKET=`echo ${FILE_SETTINGS__LOWERCASE_BUCKET:=false} | tr -d \"`
 
 ##################
 # Email Settings #
 ##################
-export ENABLE_SIGNUP_WITH_EMAIL=`echo ${ENABLE_SIGNUP_WITH_EMAIL:=false} | tr -d \"`
-export SEND_EMAIL_NOTIFICATIONS=`echo ${SEND_EMAIL_NOTIFICATIONS:=false} | tr -d \"`
-export REQUIRE_EMAIL_VERIFICATION=`echo ${REQUIRE_EMAIL_VERIFICATION:=false} | tr -d \"`
 export EMAIL_SETTINGS__INVITE_SALT=$(printf "${EMAIL_SETTINGS__INVITE_SALT}%.0s" {1..32} | cut -c -32)
 export EMAIL_SETTINGS__PASSWORD_RESET_SALT=$(printf "${EMAIL_SETTINGS__PASSWORD_RESET_SALT}%.0s" {1..32} | cut -c -32)
-export PUSH_NOTIFICATION_CONTENTS=${PUSH_NOTIFICATION_CONTENTS:="full"}
-export SEND_PUSH_NOTIFICATIONS=`echo ${SEND_PUSH_NOTIFICATIONS:=false} | tr -d \"`
 
+export ENABLE_SIGNUP_WITH_EMAIL=`echo ${ENABLE_SIGNUP_WITH_EMAIL:=true} | tr -d \"`
+export ENABLE_SIGNIN_WITH_EMAIL=`echo ${ENABLE_SIGNIN_WITH_EMAIL:=true} | tr -d \"`
+export ENABLE_SIGNIN_WITH_USERNAME=`echo ${ENABLE_SIGNIN_WITH_USERNAME:=true} | tr -d \"`
+export SEND_EMAIL_NOTIFICATIONS=`echo ${SEND_EMAIL_NOTIFICATIONS:=false} | tr -d \"`
+export REQUIRE_EMAIL_VERIFICATION=`echo ${REQUIRE_EMAIL_VERIFICATION:=false} | tr -d \"`
+export SEND_PUSH_NOTIFICATIONS=`echo ${SEND_PUSH_NOTIFICATIONS:=false} | tr -d \"`
+export ENABLE_EMAIL_BATCHING=`echo ${ENABLE_EMAIL_BATCHING:=false} | tr -d \"` 
+export EMAIL_BATCHING_BUFFER_SIZE=`echo ${EMAIL_BATCHING_BUFFER_SIZE:=256} | tr -d \"` 
+export EMAIL_BATCHING_INTERVAL=`echo ${EMAIL_BATCHING_INTERVAL:=30} | tr -d \"` 
 
 #######################
 # Rate Limit Settings #
 #######################
 export VARY_BY_REMOTE_ADDR=`echo ${VARY_BY_REMOTE_ADDR:=false} | tr -d \"`
+export ENABLE_RATE_LIMITER=`echo ${ENABLE_RATE_LIMITER:=true} | tr -d \"`
+export RATE_PER_SEC=`echo ${RATE_PER_SEC:=10} | tr -d \"`
+export RATELIMIT_MEM_STORE_SIZE=`echo ${RATELIMIT_MEM_STORE_SIZE:=10000} | tr -d \"`
 
+####################
+# Privacy Settings #
+####################
+export SHOW_EMAIL_ADDRESS=`echo ${SHOW_EMAIL_ADDRESS:=true} | tr -d \"`
+export SHOW_FULL_NAME=`echo ${SHOW_FULL_NAME:=true} | tr -d \"`
+
+#####################
+# Password Settings #
+#####################
+export PASSWORD_MININMUM_LENGTH=`echo ${PASSWORD_MININMUM_LENGTH:=8} | tr -d \"`
+export PASSWORD_LOWERCASE=`echo ${PASSWORD_LOWERCASE:=false} | tr -d \"`
+export PASSWORD_NUMBER=`echo ${PASSWORD_NUMBER:=false} | tr -d \"`
+export PASSWORD_UPEPRCASE=`echo ${PASSWORD_UPEPRCASE:=false} | tr -d \"`
+export PASSWORD_SYMBOL=`echo ${PASSWORD_SYMBOL:=false} | tr -d \"`
+
+##########################
+# SSO/LDAP/SAML Settings #
+##########################
+export ENABLE_GITLAB=`echo ${ENABLE_GITLAB:=false} | tr -d \"`
+export ENABLE_GOOGLE=`echo ${ENABLE_GOOGLE:=false} | tr -d \"`
+export ENABLE_OFFICE365=`echo ${ENABLE_OFFICE365:=false} | tr -d \"`
+
+export ENABLE_LDAP=`echo ${ENABLE_LDAP:=false} | tr -d \"`
+export LDAP_PORT=`echo ${LDAP_PORT:=389} | tr -d \"`
+export LDAP_SYNC_INTERVAL=`echo ${LDAP_SYNC_INTERVAL:=60} | tr -d \"`
+export LDAP_SKIP_CERT_VERIFICATION=`echo ${LDAP_SKIP_CERT_VERIFICATION:=false} | tr -d \"`
+export LDAP_QUERY_TIMEOUT=`echo ${LDAP_QUERY_TIMEOUT:=60} | tr -d \"`
+export LDAP_MAX_PAGE_SIZE=`echo ${LDAP_MAX_PAGE_SIZE:=0} | tr -d \"`
+
+export ENABLE_SAML=`echo ${ENABLE_SAML:=false} | tr -d \"`
+export ENABLE_SAML_VERIFY=`echo ${ENABLE_SAML_VERIFY:=false} | tr -d \"`
+export ENABLE_SAML_ENCRYPT=`echo ${ENABLE_SAML_ENCRYPT:=false} | tr -d \"`
+
+#######################
+# Compliance Settings #
+#######################
+export ENABLE_COMPLIANCE=`echo ${ENABLE_COMPLIANCE:=false} | tr -d \"`
+export ENABLE_DAILY_COMPLIANCE=`echo ${ENABLE_DAILY_COMPLIANCE:=false} | tr -d \"`
+
+####################
+# Cluster Settings #
+####################
+export ENABLE_CLUSTER=`echo ${ENABLE_CLUSTER:=false} | tr -d \"`
+export CLUSTER_INTER_NODE_URLS=`echo ${CLUSTER_INTER_NODE_URLS:1:${#CLUSTER_INTER_NODE_URLS}-2}`
 
 ########################################################################
 # Write Config variables in envrionment to the configuration JSON file #
